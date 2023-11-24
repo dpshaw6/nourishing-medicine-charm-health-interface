788dd18d24a917c1d5328785618bdf1a497ff89b
Daniel Shaw <dpshaw@gmail.com>
2023-11-24 09:29:43 -0800

    Creation of Mock Data. Test access of Mock Ingredients.

---
+++ frontend/src/components/IngredientsList.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import mockIngredients from '../mock_data/ingredients.json'; 
+
+const IngredientsList = () => {
+    const [ingredients, setIngredients] = useState(mockIngredients);
+
+    return (
+        <div>
+            {ingredients.map(ingredient => (
+                <div key={ingredient.id}>{ingredient.name} - ${ingredient.costPerGram}/g</div>
+            ))}
+        </div>
+    );
+}
+
+export default IngredientsList;
