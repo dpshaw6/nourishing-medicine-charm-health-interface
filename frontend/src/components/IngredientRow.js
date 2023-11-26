6b55785f264101574fb2ace8b2b3fb113f3524c5
Daniel Shaw <dpshaw@gmail.com>
2023-11-25 17:08:24 -0800

    Cost Calculations (per row) working

--- frontend/src/components/IngredientRow.js
+++ frontend/src/components/IngredientRow.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import mockIngredients from '../data/ingredients.json';
 
 const IngredientRow = ({ rowId, ingredientId, relativeAmount, totalMass, totalRelativeAmount, onRelativeAmountChange, onDelete, onIngredientChange }) => {
+    const [overrideMarkup, setOverrideMarkup] = useState(false);
+    const [markup, setMarkup] = useState(2.5); // Default markup
+
     const ingredient = mockIngredients.find(ing => ing.id === ingredientId);
-    const defaultMarkup = 2.5; // Default markup
-    const markup = defaultMarkup; // This can be dynamic if override is implemented
 
     const handleRelativeAmountChange = (e) => {
         onRelativeAmountChange(Number(e.target.value));
@@ -14,10 +15,27 @@ const IngredientRow = ({ rowId, ingredientId, relativeAmount, totalMass, totalRe
         onIngredientChange(e.target.value);
     };
 
+    const handleMarkupChange = (e) => {
+        setMarkup(Number(e.target.value));
+    };
+
+    const handleOverrideChange = () => {
+        setOverrideMarkup(!overrideMarkup);
+        if (!overrideMarkup) {
+            setMarkup(2.5); // Reset to default if override is unchecked
+        }
+    };
+
     // Calculate Absolute Amount
     const absoluteAmount = totalRelativeAmount > 0
                             ? (relativeAmount / totalRelativeAmount) * totalMass
                             : 0;
+
+    // Calculate Provider Cost
+    const providerCost = absoluteAmount * ingredient.costPerGram;
+
+    // Calculate Patient Cost
+    const patientCost = providerCost * markup;
 
     return (
         <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', margin: '5px 0' }}>
@@ -28,18 +46,24 @@ const IngredientRow = ({ rowId, ingredientId, relativeAmount, totalMass, totalRe
             </select>
             <span>{absoluteAmount.toFixed(2)}g</span>
             <span>${ingredient.costPerGram}/g</span>
-            <span>{markup}</span>
-            {/* Placeholder for Override Checkbox */}
-            <input type="checkbox" />
+            <input 
+                type="checkbox" 
+                checked={overrideMarkup}
+                onChange={handleOverrideChange}
+            />
+            <input 
+                type="number"
+                value={markup}
+                onChange={handleMarkupChange}
+                disabled={!overrideMarkup}
+            />
             <input 
                 type="number" 
                 value={relativeAmount} 
                 onChange={handleRelativeAmountChange}
             />
-            {/* Placeholder for Provider Cost */}
-            <span>$0.00</span>
-            {/* Placeholder for Patient Cost */}
-            <span>$0.00</span>
+            <span>${providerCost.toFixed(2)}</span>
+            <span>${patientCost.toFixed(2)}</span>
             <button onClick={() => onDelete(rowId)}>Delete</button>
         </div>
     );
